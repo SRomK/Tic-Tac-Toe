302e72331f974780f47a85da2d5543a1bc427637
Sofia <sofiaxrk@gmail.com>
2022-02-01 14:50:14 +0000

    guardando 16h

--- src/js/view/main.jsx
+++ src/js/view/main.jsx
@@ -2,23 +2,61 @@ import React, { useState } from "react";
 import Circles from "../component/circles.jsx";
 import Crosses from "../component/crosses.jsx";
 
-//include images into your bundle
 import "../../styles/main.css";
 
 //create your first component
 //mi idea es usar las clases de los componentes de circulo y cruz
+/*const [count, setCount] = useState(0);
 
-//const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me*/
+//const [player, setPlayer] = useState();
+//player == "" ? setImage : (else) otra cosa
+//if(//player == "" ? setImage : (else) otra cosa)
 
 const Main = () => {
-	const [image, setImage] = useState();
-	//const [player, setPlayer] = useState();
-	//player == "" ? setImage : (else) otra cosa
+	const [turn, setTurn] = useState(null);
+	const [player, setPlayer] = useState("");
+	const [matrix, setMatrix] = useState([
+		[undefined, undefined, undefined],
+		[undefined, undefined, undefined],
+		[undefined, undefined, undefined],
+	]);
+
+	const selectTurn = () => {
+		if (turn == <Circles /> && player == "") {
+			turn == <Crosses />;
+		}
+		if (turn == <Crosses /> && player == "") {
+			turn == <Circles />;
+		}
+	};
+
+	const myAlert = () =>
+		alert("Comienza el juego! :) Empiezan los gatitos!>^.^<");
+
+	const calculateWinner = (squares) => {
+		const winnerLines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 4, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 4, 6],
+			[2, 5, 8],
+		];
+	};
 
 	return (
 		<>
 			<div>
-				<button className="start">Start</button>
+				<button className="start" onClick={myAlert}>
+					Start
+				</button>
 				<button className="restart">Restart</button>
 
 				<table className="tablaContainer">
@@ -26,33 +64,70 @@ const Main = () => {
 						<tr>
 							<td
 								onClick={() => {
-									//if(	//player == "" ? setImage : (else) otra cosa)
-									setImage(<Circles />);
+									setTurn(selectTurn());
 								}}>
-								{image}
+								{turn}
 							</td>
 
-							<td>
-								<Crosses />
+							<td
+								onClick={() => {
+									setTurn(selectTurn());
+								}}>
+								{turn}
 							</td>
 
-							<td>Celda 3</td>
+							<td
+								onClick={() => {
+									setTurn(selectTurn());
+								}}>
+								{turn}
+							</td>
 						</tr>
 
 						<tr>
-							<td>Celda 4</td>
+							<td
+								onClick={() => {
+									setTurn(selectTurn());
+								}}>
+								{turn}
+							</td>
 
-							<td>Celda 5</td>
+							<td
+								onClick={() => {
+									setTurn(selectTurn());
+								}}>
+								{turn}
+							</td>
 
-							<td>Celda 6</td>
+							<td
+								onClick={() => {
+									setTurn(selectTurn());
+								}}>
+								{turn}
+							</td>
 						</tr>
 
 						<tr>
-							<td>Celda 7</td>
+							<td
+								onClick={() => {
+									setTurn(selectTurn());
+								}}>
+								{turn}
+							</td>
 
-							<td>Celda 8</td>
+							<td
+								onClick={() => {
+									setTurn(selectTurn());
+								}}>
+								{turn}
+							</td>
 
-							<td>Celda 9</td>
+							<td
+								onClick={() => {
+									setTurn(selectTurn());
+								}}>
+								{turn}
+							</td>
 						</tr>
 					</tbody>
 				</table>
